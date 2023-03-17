b53e18630461acb48bbe9316992d5a440a424c68
Maria <17mashmallow@gmail.com>
2023-03-18 02:57:05 +0300

    added changed form

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,7 @@ import './App.scss';
 
 import Header from '../Header/Header';
 import ChooseTourSection from '../ChooseTourSection/ChooseTourSection';
-// import Form from './components/FormSection/Form';
+import Form from '../Form/Form';
 // import Reviews from './components/ReviewsSection/Reviews/Reviews';
 // import TravelPhoto from './components/TravelPhotoSection/TravelPhoto';
 // import TravelStories from './components/TravelStoriesSection/TravelStories/TravelStories';
@@ -14,7 +14,7 @@ function App() {
     <div className="wrapper">
       <Header />
       <ChooseTourSection />
-      {/* <Form /> */}
+      <Form />
       {/* <Reviews /> */}
       {/* <TravelPhoto /> */}
       {/* <TravelStories /> */}
