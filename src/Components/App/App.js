044ba5322ae3ff9bfac70b1df94d651cbc201da4
Maria <17mashmallow@gmail.com>
2023-03-18 03:10:25 +0300

    added changed reviews

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,7 +3,7 @@ import './App.scss';
 import Header from '../Header/Header';
 import ChooseTourSection from '../ChooseTourSection/ChooseTourSection';
 import Form from '../Form/Form';
-// import Reviews from './components/ReviewsSection/Reviews/Reviews';
+import ReviewsSection from '../ReviewsSection/ReviewsSection';
 // import TravelPhoto from './components/TravelPhotoSection/TravelPhoto';
 // import TravelStories from './components/TravelStoriesSection/TravelStories/TravelStories';
 // import MailSection from './components/MailSection/MailSection';
@@ -15,7 +15,7 @@ function App() {
       <Header />
       <ChooseTourSection />
       <Form />
-      {/* <Reviews /> */}
+      <ReviewsSection />
       {/* <TravelPhoto /> */}
       {/* <TravelStories /> */}
       {/* <MailSection /> */}
