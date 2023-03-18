31a30fba53ac0644f65c0eb0afc7523c65125041
Maria <17mashmallow@gmail.com>
2023-03-18 03:18:15 +0300

    added changed travel ph section

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,6 +4,7 @@ import Header from '../Header/Header';
 import ChooseTourSection from '../ChooseTourSection/ChooseTourSection';
 import Form from '../Form/Form';
 import ReviewsSection from '../ReviewsSection/ReviewsSection';
+import TravelPhotoSection from '../TravelPhotoSection/TravelPhotoSection';
 // import TravelPhoto from './components/TravelPhotoSection/TravelPhoto';
 // import TravelStories from './components/TravelStoriesSection/TravelStories/TravelStories';
 // import MailSection from './components/MailSection/MailSection';
@@ -16,7 +17,7 @@ function App() {
       <ChooseTourSection />
       <Form />
       <ReviewsSection />
-      {/* <TravelPhoto /> */}
+      <TravelPhotoSection />
       {/* <TravelStories /> */}
       {/* <MailSection /> */}
       {/* <Footer /> */}
