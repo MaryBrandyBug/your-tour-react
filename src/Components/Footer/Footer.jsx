7ec3e8ca0e66beaef09047bbd1cc2a463ac2946f
Maria Bystrova <17mashmallow@gmail.com>
2023-07-18 02:15:32 +0300

    change class in footer block in style module

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -11,15 +11,15 @@ export default function Footer() {
       <div className={cx('footer__content')}>
         <p className={cx('footer__content__text')}>Наши социальные сети</p>
         <div className={cx('footer__content__social-networks-links')}>
-          <div className={cx('footer__link-block')}>
+          <div className={cx('footerLinkBlock')}>
             <div className={cx('footer__link-icon', 'inst-icon')}><img src="/your-tour-react/images/inst.png" alt="instagram icon" /></div>
             <Link className={cx('footer__social-network')} to="/">instagram</Link>
           </div>
-          <div className={cx('footer__link-block')}>
+          <div className={cx('footerLinkBlock')}>
             <div className={cx('footer__link-icon')}><img src="/your-tour-react/images/fb.png" alt="facebook icon" /></div>
             <Link className={cx('footer__social-network')} to="/">facebook</Link>
           </div>
-          <div className={cx('footer__link-block')}>
+          <div className={cx('footerLinkBlock')}>
             <div className={cx('footer__link-icon')}><img src="/your-tour-react/images/vk.png" alt="vkontakte icon" /></div>
             <Link className={cx('footer__social-network')} to="/">vkontakte</Link>
           </div>
