9c115e7cbc5724fb0ad3dbd544cf940a50ec96c7
Maria Bystrova <17mashmallow@gmail.com>
2023-07-28 02:23:59 +0300

    header component classnames change

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -12,8 +12,8 @@ export default function Header() {
   const menuItems = [{ item: 'Туры', id: 'chooseTour' }, { item: 'Создать тур', id: 'createTour' }, { item: 'Отзывы', id: 'reviews' }, { item: 'Истории', id: 'travelStories' }];
   const menu = menuItems.map((i, index) => <HashLink to={`#${i.id}`} key={index} className={cx('menu__item')} style={{ textDecoration: 'none' }}>{i.item}</HashLink>);
   return (
-    <header className={cx('header')}>
-      <div className={cx('header__navbar')}>
+    <header className={cx('root')}>
+      <div className={cx('navbar__container')}>
         <div className={cx('navbar')}>
           <div className={cx('navbar__logo')}>
             <img src="/your-tour-react/images/YourTour.png" alt="logo" className={cx('YourTourLogo')} />
@@ -25,10 +25,10 @@ export default function Header() {
           <Link to="tel:+79999999999" className={cx('telephone')}>+7 999 999 99 99</Link>
         </div>
       </div>
-      <div className={cx('header__content')}>
-        <h1 className={cx('header__content__title')}>Идеальные путешествия существуют</h1>
-        <p className={cx('header__content__subtitle')}>Идейные соображения высшего порядка, а также рамки и место обучения кадров</p>
-        <div className={cx('header__content__button')}>
+      <div className={cx('content')}>
+        <h1 className={cx('content__title')}>Идеальные путешествия существуют</h1>
+        <p className={cx('content__subtitle')}>Идейные соображения высшего порядка, а также рамки и место обучения кадров</p>
+        <div className={cx('content__button')}>
           <Button text="Найти тур" btnClass={cx('find-tour-btn')} />
         </div>
       </div>
