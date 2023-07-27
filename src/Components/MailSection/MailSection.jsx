28c18d9cd66064eed6f404cf95c572ab2b6ee6a0
Maria Bystrova <17mashmallow@gmail.com>
2023-07-28 02:27:26 +0300

    mailSection component classnames change

--- src/Components/MailSection/MailSection.jsx
+++ src/Components/MailSection/MailSection.jsx
@@ -7,14 +7,14 @@ export default function MailSection() {
   const cx = classNames.bind(styles);
 
   return (
-    <div className={cx('mail-section')}>
-      <img src="/your-tour-react/images/footerPhoto.png" alt="beautiful view" className={cx('mail-section__photo')} />
-      <div className={cx('mail-section__content')}>
-        <h3 className={cx('mail-section__content__header')}>Пора в путешествие вместе с нами!</h3>
-        <p className={cx('mail-section__content__text')}>
+    <div className={cx('root')}>
+      <img src="/your-tour-react/images/footerPhoto.png" alt="beautiful view" className={cx('photo')} />
+      <div className={cx('content')}>
+        <h3 className={cx('content__header')}>Пора в путешествие вместе с нами!</h3>
+        <p className={cx('content__text')}>
           Напиши на почту и узнай подробности на
           {' '}
-          <Link to="mailto:yourtour@gmail.com" className={cx('mail-section__content__mail')}>yourtour@gmail.com</Link>
+          <Link to="mailto:yourtour@gmail.com" className={cx('content__mail')}>yourtour@gmail.com</Link>
         </p>
       </div>
     </div>
