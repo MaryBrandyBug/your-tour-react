c5b8f91ef50be01efa55aed7270f32c50de3680e
Maria Bystrova <17mashmallow@gmail.com>
2023-08-24 23:28:52 +0400

    change styles import in app component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,5 @@
-import './App.scss';
+import classNames from 'classnames/bind';
+import styles from './App.scss';
 
 import Header from '../Header';
 import ChooseTourSection from '../ChooseTourSection';
@@ -10,8 +11,9 @@ import MailSection from '../MailSection';
 import Footer from '../Footer';
 
 function App() {
+  const cx = classNames.bind(styles);
   return (
-    <div className="wrapper">
+    <div className={cx('wrapper')}>
       <Header />
       <ChooseTourSection />
       <Form />
