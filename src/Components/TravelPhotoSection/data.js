26b94d7988c18e55184fccce4a03389e2eb357db
Maria Bystrova <17mashmallow@gmail.com>
2023-08-29 19:08:30 +0400

    change travelPhotoSection

--- src/Components/TravelPhotoSection/data.js
+++ src/Components/TravelPhotoSection/data.js
@@ -1,33 +1,39 @@
-const bigPhotoClass = 'big-photo';
-const smallPhotoClass = 'small-photo';
-const maxSize = 'max-size';
-const maxMiddleSize = 'max-middle-size';
-
-export const data1 = [{ path: '/your-tour-react/images/travelphoto-1.png', firstClass: bigPhotoClass, id: '1' },
-  {
-    path: '/your-tour-react/images/travelphoto-2.png', firstClass: bigPhotoClass, secondClass: maxSize, id: '2',
-  },
-  {
-    path: '/your-tour-react/images/travelphoto-3.png', firstClass: bigPhotoClass, secondClass: maxMiddleSize, id: '3',
-  },
-  { path: '/your-tour-react/images/travelphoto-4.png', firstClass: bigPhotoClass, id: '4' }];
-
-export const data2 = [{ path: '/your-tour-react/images/travelphoto-5.png', firstClass: smallPhotoClass, id: '1' },
-  {
-    path: '/your-tour-react/images/travelphoto-6.png', firstClass: smallPhotoClass, secondClass: maxMiddleSize, id: '2',
-  },
-  {
-    path: '/your-tour-react/images/travelphoto-7.png', firstClass: smallPhotoClass, secondClass: maxSize, id: '3',
-  },
-  { path: '/your-tour-react/images/travelphoto-8.png', firstClass: smallPhotoClass, id: '4' },
-  { path: '/your-tour-react/images/travelphoto-13.png', firstClass: smallPhotoClass, id: '5' }];
-
-export const data3 = [{ path: '/your-tour-react/images/travelphoto-9.png', firstClass: bigPhotoClass, id: '1' },
-  { path: '/your-tour-react/images/travelphoto-10.png', firstClass: bigPhotoClass, id: '2' },
-  {
-    path: '/your-tour-react/images/travelphoto-11.png', firstClass: bigPhotoClass, secondClass: maxMiddleSize, id: '3',
-  },
-  {
-    path: '/your-tour-react/images/travelphoto-12.png', firstClass: bigPhotoClass, secondClass: maxSize, id: '4',
-  },
-];
+export default [{
+  path: '/your-tour-react/images/travelphoto-1.png', size: 'big', id: '1', width1630: true, width992: true,
+},
+{
+  path: '/your-tour-react/images/travelphoto-2.png', size: 'big', id: '2', width1630: false, width992: false,
+},
+{
+  path: '/your-tour-react/images/travelphoto-3.png', size: 'big', id: '3', width1630: true, width992: false,
+},
+{
+  path: '/your-tour-react/images/travelphoto-4.png', size: 'big', id: '4', width1630: true, width992: true,
+},
+{
+  path: '/your-tour-react/images/travelphoto-5.png', size: 'small', id: '5', width1630: true, width992: true,
+},
+{
+  path: '/your-tour-react/images/travelphoto-6.png', size: 'small', id: '6', width1630: true, width992: false,
+},
+{
+  path: '/your-tour-react/images/travelphoto-7.png', size: 'small', id: '7', width1630: false, width992: false,
+},
+{
+  path: '/your-tour-react/images/travelphoto-8.png', size: 'small', id: '8', width1630: true, width992: true,
+},
+{
+  path: '/your-tour-react/images/travelphoto-13.png', size: 'small', id: '9', width1630: true, width992: true,
+},
+{
+  path: '/your-tour-react/images/travelphoto-9.png', size: 'big', id: '10', width1630: true, width992: true,
+},
+{
+  path: '/your-tour-react/images/travelphoto-10.png', size: 'big', id: '11', width1630: true, width992: true,
+},
+{
+  path: '/your-tour-react/images/travelphoto-11.png', size: 'big', id: '12', width1630: true, width992: false,
+},
+{
+  path: '/your-tour-react/images/travelphoto-12.png', size: 'big', id: '13', width1630: false, width992: false,
+}];
