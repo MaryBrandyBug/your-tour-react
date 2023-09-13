f31ab28a9f34ee31698b0eecd494d9dfcfae53c2
Maria Bystrova <17mashmallow@gmail.com>
2023-09-13 19:51:19 +0400

    change footer icons

--- src/Components/Footer/data.js
+++ src/Components/Footer/data.js
@@ -1,9 +1,9 @@
 export default [{
-  id: '1', img: '/your-tour-react/images/inst.png', alt: 'instagram icon', link: '/', text: 'instagram',
+  id: '1', img: '/your-tour-react/images/inst.svg', alt: 'instagram icon', link: '/', text: 'instagram',
 },
 {
-  id: '2', img: '/your-tour-react/images/fb.png', alt: 'facebook icon', link: '/', text: 'facebook',
+  id: '2', img: '/your-tour-react/images/fb.svg', alt: 'facebook icon', link: '/', text: 'facebook',
 },
 {
-  id: '3', img: '/your-tour-react/images/vk.png', alt: 'vkontakte icon', link: '/', text: 'vkontakte',
+  id: '3', img: '/your-tour-react/images/vk.svg', alt: 'vkontakte icon', link: '/', text: 'vkontakte',
 }];
